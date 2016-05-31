c11f4c63352f12303652fdb7ae771750e32d9bbe
Oleksandr Bardanov <oleksandr.bardanov@avid.com>
2016-05-31 14:46:21 +0300

    show connection status

--- public/app.js
+++ public/app.js
@@ -3,7 +3,17 @@
 const protocol = location.href.indexOf('https://') !== -1 ? 'wss' : 'ws';
 
 const ws = new WebSocket(`${protocol}://${location.host}`);
+
 const counterElement = document.querySelector('#counter');
+const connectElement = document.querySelector('#connect');
+
+ws.onopen = () => {
+    connectElement.textContent = 'Connected.'
+}
+
+ws.onclose = () => {
+    connectElement.textContent = 'Disconnected.'
+}
 
 ws.onmessage = message => {
     var data = JSON.parse(message.data);
