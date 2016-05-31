13cde7740afb2bbc6dc570d214f98cd84d516dd3
Oleksandr Bardanov <oleksandr.bardanov@avid.com>
2016-05-31 14:52:07 +0300

    calculate average latency

--- public/app.js
+++ public/app.js
@@ -5,6 +5,7 @@ const protocol = location.href.indexOf('https://') !== -1 ? 'wss' : 'ws';
 const ws = new WebSocket(`${protocol}://${location.host}`);
 
 const counterElement = document.querySelector('#counter');
+const averageElement = document.querySelector('#average');
 const connectElement = document.querySelector('#connect');
 
 ws.onopen = () => {
@@ -15,12 +16,17 @@ ws.onclose = () => {
     connectElement.textContent = 'Disconnected.'
 }
 
+const latencyData = [];
+
 ws.onmessage = message => {
     var data = JSON.parse(message.data);
 
     if (data.type === 'first'){
         ws.send(data.id);
     } else {
+        latencyData.push(data.latency);
+
         counterElement.textContent = `${data.latency}ms`;
+        averageElement.textContent = `${(latencyData.reduce((a, b) => { return a + b; }) / latencyData.length).toFixed()}ms`;
     }
 }
